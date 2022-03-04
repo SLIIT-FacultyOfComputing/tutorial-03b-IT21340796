336564eb28db30988be2bd9c1eab3703bdd713d3
IT21340796 <99330257+IT21340796@users.noreply.github.com>
2022-03-04 07:42:50 +0000

    I did tutorial 3

--- exercise04.cpp
+++ exercise04.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-void print(int &len, int &wth);
-void input(int &len, int &wth);
+void print(int len, int wth);
+void input(int &length, int &width);
 
 
 // Do not change the main() function
@@ -15,17 +15,17 @@ int main() {
 }
 
 // Do not change the print() function
-void print(int &len, int &wth) {
+void print(int len, int wth) {
    cout << "Length : " << len 
         << " ,Width  : " << wth << endl;
 }
 
 // Implement the Input Function here
-void input(int &len, int &wth)
+void input(int &length, int &width)
 {
    cout << "Length : "<< endl ;
-  cin>>len ;
+  cin>>length ;
    cout << "Width : " << endl ;
-  cin>>wth ;
+  cin>>width ;
   
 }
